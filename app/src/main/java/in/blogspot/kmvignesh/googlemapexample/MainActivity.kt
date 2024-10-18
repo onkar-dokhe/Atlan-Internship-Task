eec18625acf6679a22bb567d84fac822d20ed27e
users86245 <130378380+users86245@users.noreply.github.com>
2024-10-18 22:26:33 +0530

    Final Code

---
+++ app/src/main/java/in/blogspot/kmvignesh/googlemapexample/MainActivity.kt
@@ -0,0 +1,155 @@
+package `in`.blogspot.kmvignesh.googlemapexample
+
+import android.graphics.Color
+import android.os.AsyncTask
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.maps.model.PolylineOptions
+import com.google.gson.Gson
+import okhttp3.OkHttpClient
+import okhttp3.Request
+
+class MainActivity : AppCompatActivity() {
+
+
+    lateinit var mapFragment : SupportMapFragment
+    lateinit var googleMap: GoogleMap
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(OnMapReadyCallback {
+            googleMap = it
+            Log.d("GoogleMap", "before isMyLocationEnabled")
+//            googleMap.isMyLocationEnabled = true
+            val location1 = LatLng(28.520510,77.200580)
+            googleMap.addMarker(MarkerOptions().position(location1).title("Mio Coworks"))
+            googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(location1,15f))
+
+            Log.d("GoogleMap", "before location2")
+            val location2 = LatLng(18.630110,73.778160)
+            googleMap.addMarker(MarkerOptions().position(location2).title("Onkar Home"))
+
+            Log.d("GoogleMap", "before location3")
+
+            val location3 = LatLng(28.515630,77.198060)
+            googleMap.addMarker(MarkerOptions().position(location3).title("Atlan Office"))
+
+            Log.d("GoogleMap", "before URL")
+
+
+
+
+            val URL = getDirectionURL(location1,location3)
+            Log.d("GoogleMap", "URL : $URL")
+            GetDirection(URL).execute()
+
+
+            val polylineOptions = PolylineOptions()
+                .add(location2)
+                .add(location3)
+                .width(10f)
+                .color(Color.BLACK)
+                .geodesic(true)
+
+            // Add the polyline to the Google Map
+            googleMap.addPolyline(polylineOptions)
+
+        })
+    }
+
+//    fun getDirectionURL(origin:LatLng,dest:LatLng) : String{
+//        return "https://maps.googleapis.com/maps/api/directions/json?origin=${origin.latitude},${origin.longitude}&destination=${dest.latitude},${dest.longitude}&sensor=false&mode=driving"
+//    }
+
+    fun getDirectionURL(origin: LatLng, dest: LatLng): String {
+        val apiKey = "AIzaSyCNCH0dFuL0_LiDB_ck1n5kolto8huMxFQ"
+        return "https://maps.googleapis.com/maps/api/directions/json?origin=${origin.latitude},${origin.longitude}&destination=${dest.latitude},${dest.longitude}&sensor=false&mode=driving&key=$apiKey"
+    }
+
+    private inner class GetDirection(val url : String) : AsyncTask<Void,Void,List<List<LatLng>>>(){
+        override fun doInBackground(vararg params: Void?): List<List<LatLng>> {
+            val client = OkHttpClient()
+            val request = Request.Builder().url(url).build()
+            val response = client.newCall(request).execute()
+            val data = response.body!!.string()
+            Log.d("GoogleMap" , " data : $data")
+            val result =  ArrayList<List<LatLng>>()
+            try{
+                val respObj = Gson().fromJson(data,GoogleMapDTO::class.java)
+
+                val path =  ArrayList<LatLng>()
+
+                for (i in 0..(respObj.routes[0].legs[0].steps.size-1)){
+//                    val startLatLng = LatLng(respObj.routes[0].legs[0].steps[i].start_location.lat.toDouble()
+//                            ,respObj.routes[0].legs[0].steps[i].start_location.lng.toDouble())
+//                    path.add(startLatLng)
+//                    val endLatLng = LatLng(respObj.routes[0].legs[0].steps[i].end_location.lat.toDouble()
+//                            ,respObj.routes[0].legs[0].steps[i].end_location.lng.toDouble())
+                    path.addAll(decodePolyline(respObj.routes[0].legs[0].steps[i].polyline.points))
+                }
+                result.add(path)
+            }catch (e:Exception){
+                e.printStackTrace()
+            }
+            return result
+        }
+
+        override fun onPostExecute(result: List<List<LatLng>>) {
+            val lineoption = PolylineOptions()
+            for (i in result.indices){
+                lineoption.addAll(result[i])
+                lineoption.width(15f)
+                lineoption.color(Color.BLACK)
+                lineoption.geodesic(true)
+            }
+            googleMap.addPolyline(lineoption)
+        }
+    }
+
+    public fun decodePolyline(encoded: String): List<LatLng> {
+
+        val poly = ArrayList<LatLng>()
+        var index = 0
+        val len = encoded.length
+        var lat = 0
+        var lng = 0
+
+        while (index < len) {
+            var b: Int
+            var shift = 0
+            var result = 0
+            do {
+                b = encoded[index++].toInt() - 63
+                result = result or (b and 0x1f shl shift)
+                shift += 5
+            } while (b >= 0x20)
+            val dlat = if (result and 1 != 0) (result shr 1).inv() else result shr 1
+            lat += dlat
+
+            shift = 0
+            result = 0
+            do {
+                b = encoded[index++].toInt() - 63
+                result = result or (b and 0x1f shl shift)
+                shift += 5
+            } while (b >= 0x20)
+            val dlng = if (result and 1 != 0) (result shr 1).inv() else result shr 1
+            lng += dlng
+
+            val latLng = LatLng((lat.toDouble() / 1E5),(lng.toDouble() / 1E5))
+            poly.add(latLng)
+        }
+
+        return poly
+    }
+}
